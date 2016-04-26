57d9f39b310e476f1c20ede3a15ece8f0ec643bc
Kungsgeten <sjostrand.erik@gmail.com>
2016-04-26 08:49:04 +0200

    Applied changes suggested by flycheck-package

--- selected.el
+++ selected.el
@@ -1,20 +1,24 @@
-;; selected.el --- Keymap for when region is active
+;;; selected.el --- Keymap for when region is active
+
 ;; Copyright (C) 2016 Erik Sjöstrand
 ;; MIT License
-;;
+
+;;; Commentary:
+
 ;; When `selected-minor-mode' is active, the keybindings in `selected-keymap'
-;; will be enabled when tne region is active. This is useful for commands that
+;; will be enabled when tne region is active.  This is useful for commands that
 ;; operates on the region, which you only want keybound when the region is
 ;; active.
 ;;
-;; `selected-keymap' has no default bindings. Bind it yourself:
+;; `selected-keymap' has no default bindings.  Bind it yourself:
 ;; (define-key selected-keymap (kbd "u") #'upcase-region)
 
+;;; Code:
 (defvar selected-keymap (make-sparse-keymap)
-  "Keymap for `selected-minor-mode'. Add keys here that should be active when region is active.")
+  "Keymap for `selected-minor-mode'.  Add keys here that should be active when region is active.")
 
 (define-minor-mode selected-region-active
-  "Meant to activate when region becomes active. Not intended for the user. Use `selected-minor-mode'."
+  "Meant to activate when region becomes active.  Not intended for the user.  Use `selected-minor-mode'."
   :keymap selected-keymap
   (when selected-region-active
     (let ((major-selected-map
@@ -31,7 +35,7 @@
   (selected-region-active 1))
 
 (defun selected-off ()
-  "Disables bindings in `selected-keymap' temporary."
+  "Disable bindings in `selected-keymap' temporarily."
   (interactive)
   (selected-region-active -1))
 
@@ -40,11 +44,12 @@
   "If enabled activates the `selected-keymap' when the region is active."
   :lighter " sel"
   (if selected-minor-mode
-      (progn 
+      (progn
         (add-hook 'activate-mark-hook #'selected--on)
         (add-hook 'deactivate-mark-hook #'selected-off))
     (remove-hook 'activate-mark-hook #'selected--on)
     (remove-hook 'deactivate-mark-hook #'selected-off)
-    (selected--off)))
+    (selected-off)))
 
 (provide 'selected)
+;;; selected.el ends here
